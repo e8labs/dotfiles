7d1c7a2e43a083fb58c9b1d9ce11e80b25e16f3a
Eric Baranowski <git@e8labs.net>
2018-11-01 04:33:14 -0600

    added casks and formulae

---
+++ scripts/node_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit -o pipefail -o nounset
+
+echo "Updating npm dependencies."
+npm install -g elm elm-live elm-format gulp gulp-elm
+npm install -g typescript gulp-typescript tern
+npm install -g ios-deploy
+npm install -g npm
